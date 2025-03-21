e85ba41bfe0768a92c4cb18a7d50023724f75693
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-03-22 02:49:16 +0500

    code is added

--- react-query-demo/src/components/RQSuperHeros.page.js
+++ react-query-demo/src/components/RQSuperHeros.page.js
@@ -7,13 +7,20 @@ export default function RQSuperHeroesPage() {
   // It takes two arguments.. one is key and other which return a promise...
   // {isLoading, data} =>React Query ka response object
   //useQuery is a ReactHook..., and key 'super-heroes is a cache..
-  const {isLoading, data, isError, error, isFetching} = 
+  const {isLoading, data, isError, error, isFetching, refetch} = 
   useQuery({
     queryKey: ['super-heroes'], // Key array format me hamesha rakhni hai
     queryFn: () => axios.get('http://localhost:4000/superheroes'),
-    cacheTime: 5000,  // 5 sec tak data memory me rahega
-    staleTime: 30000, // 3 sec tak data fresh maana jayega
+    enabled: false,
+    // cacheTime: 5000,  // 5 sec tak data memory me rahega
+    // staleTime: 30000, // 3 sec tak data fresh maana jayega
+    // refetchOnMount: true, // Mount hone par API call nahi hogi, lkin aghr cache sy daata delete hoga fer tu hogi e..
+    // refetchOnWindowFocus: 'always', // Refetch hoga, API dubara call karega (chahe cache valid ho ya nahi).
+    // refetchInterval: 5000, // Har 5 second baad API call hogi
+    // refetchIntervalInBackground: true,  // API ko refetch karwata hai, chahe tum window focus karo ya nahi!
   });  
+
+  // console.log(data); // API ka pura response console me dikhega
 
   // defalut stale time is 0...
   // default cache time is 5minutes..
@@ -26,14 +33,13 @@ export default function RQSuperHeroesPage() {
   //     return res.data; //  Sirf resolved data return ho raha hai
   //   }
   // });
-  
 
-  // console.log(data); // API ka pura response console me dikhega
-  console.log({isLoading, isFetching});
-
-  if(isLoading){
+  if(isLoading || isFetching){
     return <h2>Loading...</h2>
   }
+
+  // console.log({isLoading, isFetching});
+
 
   if (isError) {
     return <h2>Error: {error.message}</h2>;
@@ -42,6 +48,8 @@ export default function RQSuperHeroesPage() {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      {/* UseQuery on click... */}
+      <button onClick={refetch}>Refetch Buuton</button>
       {/* data?.data likhne ki zaroorat is liye hai kyunki axios.get() ka response ek object hota hai. */}
       {data?.data.map((hero)=>{
         return <div key={hero.id}>{hero.name}</div>
